07eb1ba5617aa4972b36e447072f93f9fe1cd2d3
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-07 17:56:29 +0530

    Creating index based on input

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@ import { path } from "./api/pathAPI";
 import LongCard from "../components/longCard/LongCard";
 import HomePage from "./HomePage";
 import WideCard from "../components/subComponents/wideCard/WideCard";
+import { foundationIndex } from "./api/courseContentAPI";
+import IndexPage from "../components/IndexPage/IndexPage";
 
 export default function Home() {
-  const WideCards = path.map((pathItem, index) => {
-    return <WideCard key={index} {...pathItem} />;
-  });
-  return WideCards;
+  return <IndexPage {...foundationIndex} />;
 }
