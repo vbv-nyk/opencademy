009b7dfcdaab24ef60683746eff746c5e6e19519
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-08 03:44:35 +0530

    Add content to assignments

--- components/subComponents/assignmentCard/AssignmentCard.tsx
+++ components/subComponents/assignmentCard/AssignmentCard.tsx
@@ -5,22 +5,22 @@ export default function AssignmentCard() {
   const assignments = [
     {
       assignmentDesc:
-        "To get some practice using lists, create a new HTML document and create the following lists:",
+        "Please ensure that you have completed all of the following questions before moving on. To gain a deeper understanding of the material, it is recommended that you read the provided articles and watch the relevant videos.",
       subAssignments: [
         {
-          name: `An unordered shopping list of your favorite foods`,
-          videoURL: `https://www.youtube.com/watch?v=rIO5326FgPE`,
-          imageURL: `https://css-tricks.com/almanac/properties/m/margin/`,
+          name: `A ball is thrown with an initial velocity of 10 m/s at an angle of 45 degrees above the horizontal. Using vector addition, calculate the resulting displacement of the ball after 2 seconds.`,
+          videoURL: `https://byjus.com/physics/distance-and-displacement/`,
+          imageURL: `https://i.pinimg.com/736x/a0/80/17/a0801720139d080287ed0f456dafc124--science-revision-teaching-science.jpg`,
         },
         {
-          name: `An unordered shopping list of your favorite foods`,
-          videoURL: `https://www.youtube.com/watch?v=rIO5326FgPE`,
-          imageURL: `https://css-tricks.com/almanac/properties/m/margin/`,
+          name: `A car travels 60 km due east and then 90 km due north. Calculate the final displacement of the car from its starting point using vector subtraction.`,
+          imageURL: `https://www.youtube.com/watch?v=PQ8EDm6g-GU`,
+          videoURL: `https://www.youtube.com/watch?v=21BwUNDOQno`,
         },
         {
-          name: `An unordered shopping list of your favorite foods`,
-          videoURL: `https://www.youtube.com/watch?v=rIO5326FgPE`,
-          imageURL: `https://css-tricks.com/almanac/properties/m/margin/`,
+          name: `A vector has a magnitude of 5 units and a direction of 30 degrees. Find the x and y components of the vector using trigonometry.`,
+          videoURL: `https://www.youtube.com/watch?v=21BwUNDOQno`,
+          imageURL: `https://byjus.com/physics/distance-and-displacement/`,
         },
       ],
     },
@@ -33,12 +33,18 @@ export default function AssignmentCard() {
           <li key={id}>
             <div className={styles.assignmentInfo}>
               <div className={styles.assignmentName}>{subAssignment.name}</div>
-              <div className={styles.assignmentVideo}>
+              <a
+                className={styles.assignmentVideo}
+                href={subAssignment.videoURL}
+              >
                 {subAssignment.videoURL}
-              </div>
-              <div className={styles.assignmentImage}>
+              </a>
+              <a
+                className={styles.assignmentImage}
+                href={subAssignment.imageURL}
+              >
                 {subAssignment.imageURL}
-              </div>
+              </a>
             </div>
           </li>
         );
@@ -46,15 +52,18 @@ export default function AssignmentCard() {
     );
     return (
       <div key={id} className={styles.assignmentItem}>
-        <div>{assignmentDesc}</div>
+        <div className={styles.assignmentDesc}>{assignmentDesc}</div>
         <ul>{subAssignments}</ul>
       </div>
     );
   });
 
   return (
-    <Card>
-      <div className={styles.assignmentCard}>{assignmentsList}</div>
-    </Card>
+    <div>
+      <div className={styles.assignmentHeader}>Assignment</div>
+      <Card className={styles.assignmentCard}>
+        <div>{assignmentsList}</div>
+      </Card>
+    </div>
   );
 }
