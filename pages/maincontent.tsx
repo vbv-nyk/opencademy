295953abc60d9982cfa52fc50c6a6f0c37f9d91c
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-07 22:54:05 +0530

    adding maincontent.tsx

---
+++ pages/maincontent.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+import MainPage from "../components/MainPage/MainPage";
+
+export default function MainContent() {
+  const router = useRouter();
+  return <MainPage id={router.query.id} />;
+}
