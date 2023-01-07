b958ce110883df949fa221357053ff4811ae4d01
prathamps <pspratham16@gmail.com>
2023-01-08 00:57:43 +0530

    Design changes

--- components/MainPage/MainPage.tsx
+++ components/MainPage/MainPage.tsx
@@ -1,31 +1,23 @@
-import { foundationContent } from "../../pages/api/courseContentAPI";
-import Header from "../Header/Header";
-import styles from "./MainPage.module.css";
-import Sidebar from "./subComponents/Sidebar";
-import ReactHtmlParser, {
-  processNodes,
-  convertNodeToElement,
-  htmlparser2,
-} from "react-html-parser";
-
-import { renderToString } from "react-dom/server";
-import { hydrate } from "react-dom";
+import { foundationContent } from "../../pages/api/courseContentAPI"
+import Header from "../Header/Header"
+import styles from "./MainPage.module.css"
+import Sidebar from "./subComponents/Sidebar"
+import ReactHtmlParser from "react-html-parser"
 
 export default function MainPage(props: any) {
-  console.log(props);
-  const div = document.createElement("div");
-  const markup = props.dataPath;
-  return (
-    <div>
-      <Header />
-      <div className={styles.mainPageContainer}>
-        <div className={styles.lessonInformation}>
-          <div className={styles.mainTitle}>{props.title}</div>
-          <div className={styles.subTitle}>{props.subSection}</div>
-        </div>
-        <div>{ReactHtmlParser(markup)}</div>
-        <Sidebar />
-      </div>
-    </div>
-  );
+	const markup = props.dataPath
+	return (
+		<div className={styles.mainContainer}>
+			<div className={styles.mainPageContainer}>
+				<div className={styles.lessonInformation}>
+					<div className={styles.mainTitle}>{props.title}</div>
+					<div className={styles.subTitle}>{props.subSection}</div>
+				</div>
+				<div className={styles.ingeniousMarkUp}>{ReactHtmlParser(markup)}</div>
+				<div className={styles.sidebarContainer}>
+					<Sidebar />
+				</div>
+			</div>
+		</div>
+	)
 }
--- pages/maincontent.tsx
+++ pages/maincontent.tsx
@@ -1,14 +1,18 @@
-import { useRouter } from "next/router";
-import MainPage from "../components/MainPage/MainPage";
+import { useRouter } from "next/router"
+import MainPage from "../components/MainPage/MainPage"
+import Header from "../components/Header/Header"
 
 export default function MainContent() {
-  const router = useRouter();
-  return (
-    <MainPage
-      id={router.query.id}
-      dataPath={router.query.dataPath}
-      title={router.query.title}
-      subSection={router.query.subSection}
-    />
-  );
+	const router = useRouter()
+	return (
+		<div>
+			<Header />
+			<MainPage
+				id={router.query.id}
+				dataPath={router.query.dataPath}
+				title={router.query.title}
+				subSection={router.query.subSection}
+			/>
+		</div>
+	)
 }
