705f754c38c394c67abfb9dcad0d23157137ff63
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-07 11:20:59 +0530

    ADD Course Selection Component

---
+++ components/subComponents/streamChoiceOverlay/subcomponents/StreamModal.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button } from "antd";
+import styles from "./StreamModal.module.css";
+
+const StreamModal: React.FC = () => {
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContainer}>
+        <div className={styles.modalHeader}>Stream Choice</div>
+        <div className={styles.modalBody}>
+          <div className={styles.courseContainer}>
+            <img
+              src="https://media.discordapp.net/attachments/893444140770144267/1061022078549102663/image.png?width=367&height=423"
+              className={styles.courseImage}
+            ></img>
+            <div className={styles.courseName}>Science</div>
+          </div>
+          <div className={styles.courseContainer}>
+            <img
+              src="https://media.discordapp.net/attachments/893444140770144267/1061022078549102663/image.png?width=367&height=423"
+              className={styles.courseImage}
+            ></img>
+            <div className={styles.courseName}>Commerce</div>
+          </div>
+          <div className={styles.courseContainer}>
+            <img
+              src="https://media.discordapp.net/attachments/893444140770144267/1061022078549102663/image.png?width=367&height=423"
+              className={styles.courseImage}
+            ></img>
+            <div className={styles.courseName}>Arts</div>
+          </div>
+        </div>
+        <div className={styles.modalFooter}>For class wise..</div>
+      </div>
+    </div>
+  );
+};
+
+export default StreamModal;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import WideCard from "../components/subComponents/wideCard/WideCard";
+import StreamOverlay from "../components/subComponents/streamChoiceOverlay/StreamOverlay";
 
 export default function Home() {
-  return <WideCard />;
+  return <StreamOverlay />;
 }
