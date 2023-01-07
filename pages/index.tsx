8fdc9fb0df6acfbe295baa56c3cf9fc2748b95c4
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-07 19:48:16 +0530

    ADD functioning sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { foundationIndex } from "./api/courseContentAPI";
 import IndexPage from "../components/IndexPage/IndexPage";
 import HomePage from "./HomePage";
+import Sidebar from "../components/MainPage/subComponents/Sidebar";
+import MainPage from "../components/MainPage/MainPage";
 
 export default function Home() {
-  return <IndexPage {...foundationIndex} />;
+  return <MainPage />;
 }
